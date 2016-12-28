09d7ac66107b72fbe5cce5352aeec5a10a66b82a
capslock66 <mail.thierry.parent@gmail.com>
2016-12-28 22:39:19 +0100

    delphi 12.8 : add support for properties of type array

--- Delphi/Delphi Library/SynCommons.pas
+++ Delphi/Delphi Library/SynCommons.pas
@@ -22,9 +22,13 @@ const
   CP_UTF8 = 65001;
 
 type
+
+
+  RawUTF8 = type AnsiString;
+
   /// a CPU-dependent unsigned integer type cast of a pointer / register
   // - used for 64 bits compatibility, native under Free Pascal Compiler
-{$ifdef ISDELPHI2009}
+{$if CompilerVersion = 20} // DELPHI 2009
   PtrUInt = cardinal; { see http://synopse.info/forum/viewtopic.php?id=136 }
 {$else}
   {$ifdef UNICODE}
@@ -32,14 +36,15 @@ type
   {$else}
   PtrUInt = cardinal;
   {$endif}
-{$endif}
-  /// a CPU-dependent unsigned integer type cast of a pointer of pointer
-  // - used for 64 bits compatibility, native under Free Pascal Compiler
-  PPtrUInt = ^PtrUInt;
+{$ifend}
 
-  /// a CPU-dependent signed integer type cast of a pointer / register
-  // - used for 64 bits compatibility, native under Free Pascal Compiler
-{$ifdef ISDELPHI2009}
+/// a CPU-dependent unsigned integer type cast of a pointer of pointer
+// - used for 64 bits compatibility, native under Free Pascal Compiler
+PPtrUInt = ^PtrUInt;
+
+/// a CPU-dependent signed integer type cast of a pointer / register
+// - used for 64 bits compatibility, native under Free Pascal Compiler
+{$if CompilerVersion = 20} // DELPHI 2009
   PtrInt = integer;
 {$else}
   {$ifdef UNICODE}
@@ -47,32 +52,23 @@ type
   {$else}
   PtrInt = integer;
   {$endif}
-{$endif}
-  /// a CPU-dependent signed integer type cast of a pointer of pointer
-  // - used for 64 bits compatibility, native under Free Pascal Compiler
-  PPtrInt = ^PtrInt;
+{$ifend}
+
+/// a CPU-dependent signed integer type cast of a pointer of pointer
+// - used for 64 bits compatibility, native under Free Pascal Compiler
+PPtrInt = ^PtrInt;
 
   /// unsigned Int64 doesn't exist under older Delphi
   // - and UInt64 is buggy as hell under Delphi 2007 when inlining functions
 
-{$ifdef DELPHI6OROLDER}
-
+{$ifndef CONDITIONALEXPRESSIONS}  // Delphi 6 or newer
 // some definitions not available prior to Delphi 7
 type
   UInt64 = Int64;
 
-{$endif}
-
-{$ifdef DELPHI5OROLDER}
-  // Delphi 5 doesn't have those basic types defined :(
 const
   varShortInt = $0010;
   varInt64 = $0014; { vt_i8 }
-  soBeginning = soFromBeginning;
-  soCurrent = soFromCurrent;
-  reInvalidPtr = 2;
-  PathDelim  = '\';
-  sLineBreak = #13#10;
 
 type
   PPointer = ^Pointer;
@@ -115,117 +111,7 @@ type
   end;
 {$endif}
 
-type
-  /// a pointer to a PtrUInt array
-  TPtrUIntArray = array[0..MaxInt div SizeOf(PtrUInt)-1] of PtrUInt;
-  PPtrUIntArray = ^TPtrUIntArray;
 
-  /// a dynamic array of PtrUInt values
-  TPtrUIntDynArray = array of PtrUInt;
-
-  /// a variant values array
-  TVariantArray = array[0..MaxInt div SizeOf(Variant)-1] of Variant;
-  /// a pointer to a variant array
-  PVariantArray = ^TVariantArray;
-
-  /// a dynamic array of variant values
-  TVariantDynArray = array of variant;
-
-
-  /// RawUnicode is an Unicode String stored in an AnsiString
-  // - faster than WideString, which are allocated in Global heap (for COM)
-  // - an AnsiChar(#0) is added at the end, for having a true WideChar(#0) at ending
-  // - length(RawUnicode) returns memory bytes count: use (length(RawUnicode) shr 1)
-  // for WideChar count (that's why the definition of this type since Delphi 2009
-  // is AnsiString(1200) and not UnicodeString)
-  // - pointer(RawUnicode) is compatible with Win32 'Wide' API call
-  // - mimic Delphi 2009 UnicodeString, without the WideString or Ansi conversion overhead
-  // - all conversion to/from AnsiString or RawUTF8 must be explicit: the
-  // compiler is not able to make valid implicit conversion on CP_UTF16
-
-  RawUnicode = type AnsiString(CP_UTF16); // Codepage for an UnicodeString
-
-  /// RawUTF8 is an UTF-8 String stored in an AnsiString
-  // - use this type instead of System.UTF8String, which behavior changed
-  // between Delphi 2009 compiler and previous versions: our implementation
-  // is consistent and compatible with all versions of Delphi compiler
-  // - mimic Delphi 2009 UTF8String, without the charset conversion overhead
-  // - all conversion to/from AnsiString or RawUnicode must be explicit
-  RawUTF8 = type AnsiString;
-
-  /// WinAnsiString is a WinAnsi-encoded AnsiString (code page 1252)
-  // - use this type instead of System.String, which behavior changed
-  // between Delphi 2009 compiler and previous versions: our implementation
-  // is consistent and compatible with all versions of Delphi compiler
-  // - all conversion to/from RawUTF8 or RawUnicode must be explicit
-  WinAnsiString = type AnsiString;
-
-  {$ifdef HASCODEPAGE}
-  {$else}
-  /// define RawByteString, as it does exist in Delphi 2009+
-  // - to be used for byte storage into an AnsiString
-  // - use this type if you don't want the Delphi compiler not to do any
-  // code page conversions when you assign a typed AnsiString to a RawByteString,
-  // i.e. a RawUTF8 or a WinAnsiString
-  RawByteString = type AnsiString;
-  /// pointer to a RawByteString
-  PRawByteString = ^RawByteString;
-  {$endif}
-
-  /// SynUnicode is the fastest available Unicode native string type, depending
-  //  on the compiler used
-  // - this type is native to the compiler, so you can use Length() Copy() and
-  //   such functions with it (this is not possible with RawUnicodeString type)
-  // - before Delphi 2009+, it uses slow OLE compatible WideString
-  //   (with our Enhanced RTL, WideString allocation can be made faster by using
-  //   an internal caching mechanism of allocation buffers - WideString allocation
-  //   has been made much faster since Windows Vista/Seven)
-  // - starting with Delphi 2009, it uses fastest UnicodeString type, which
-  //   allow Copy On Write, Reference Counting and fast heap memory allocation
-  {$ifdef UNICODE}
-  SynUnicode = UnicodeString;
-  {$else}
-  SynUnicode = WideString;
-  {$endif}
-
-  PRawUnicode = ^RawUnicode;
-  PRawUTF8 = ^RawUTF8;
-  PWinAnsiString = ^WinAnsiString;
-  PWinAnsiChar = type PAnsiChar;
-  PSynUnicode = ^SynUnicode;
-
-  /// a simple wrapper to UTF-8 encoded zero-terminated PAnsiChar
-  // - PAnsiChar is used only for Win-Ansi encoded text
-  // - the Synopse mORMot framework uses mostly this PUTF8Char type,
-  // because all data is internaly stored and expected to be UTF-8 encoded
-  PUTF8Char = type PAnsiChar;
-  PPUTF8Char = ^PUTF8Char;
-
-  PObject = ^TObject;
-
-  TInt64Array = array[0..MaxInt div SizeOf(Int64)-1] of Int64;
-  PInt64Array = ^TInt64Array;
-
-  TPtrIntArray = array[0..MaxInt div SizeOf(PtrInt)-1] of PtrInt;
-  PPtrIntArray = ^TPtrIntArray;
-
-  TObjectArray = array [0..MaxInt div SizeOf(TObject)-1] of TObject;
-  PObjectArray = ^TObjectArray;
-
-  TPointerDynArray = array of pointer;
-  PPointerDynArray = ^TPointerDynArray;
-
-  TObjectDynArray = array of TObject;
-  PObjectDynArray = ^TObjectDynArray;
-
-  TCardinalArray = array[0..MaxInt div SizeOf(cardinal)-1] of cardinal;
-  PCardinalArray = ^TCardinalArray;
-
-  {$ifndef DELPHI5OROLDER}
-  /// a dynamic array of interface values
-  TInterfaceDynArray = array of IInterface;
-  PInterfaceDynArray = ^TInterfaceDynArray;
-  {$endif}
 
 
 { ************ low-level RTTI types and conversion routines ***************** }
@@ -428,7 +314,7 @@ type
     TypeInfo: PTypeInfoStored;
     Offset: PtrUInt;
   end;
-  {$ifdef ISDELPHI2010}
+  {$if CompilerVersion >= 21.0}  // DELPHI 2010
   /// map the Delphi record field enhanced RTTI (available since Delphi 2010)
   TEnhancedFieldInfo = packed record
     TypeInfo: PTypeInfoStored;
@@ -437,7 +323,7 @@ type
     NameLen: byte; // = Name[0] = length(Name)
   end;
   PEnhancedFieldInfo = ^TEnhancedFieldInfo;
-  {$endif}
+  {$ifend}
 
   TTypeInfo = packed record
     kind: TTypeKind;
@@ -468,12 +354,12 @@ type
       recSize: cardinal;
       ManagedCount: integer;
       ManagedFields: array[0..0] of TFieldInfo;
-      {$ifdef ISDELPHI2010} // enhanced RTTI containing info about all fields
+      {$if CompilerVersion >= 21.0} // DELPHI 2010 // enhanced RTTI containing info about all fields
       NumOps: Byte;
       //RecOps: array[0..0] of Pointer;
       AllCount: Integer; // !!!! may need $RTTI EXPLICIT FIELDS([vcPublic])
       AllFields: array[0..0] of TEnhancedFieldInfo;
-      {$endif ISDELPHI2010}
+      {$ifend}
     );
     tkEnumeration: (
       EnumType: TOrdType;
