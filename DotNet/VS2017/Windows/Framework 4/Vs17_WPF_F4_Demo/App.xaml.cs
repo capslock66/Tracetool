163d3f4abc4af2b9e8d38e2be4cb7dd7e8451ead
capslock66 <mail.thierry.parent@gmail.com>
2017-10-08 19:00:28 +0200

    VS2017

---
+++ DotNet/VS2017/Windows/Framework 4/Vs17_WPF_F4_Demo/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace Vs13_WPF_F4_Demo
+{
+   /// <summary>
+   /// Interaction logic for App.xaml
+   /// </summary>
+   public partial class App : Application
+   {
+   }
+}
