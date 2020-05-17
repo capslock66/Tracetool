15c189818f64d1249626cd6161302b92defb59aa
Capslock66 <mail.thierry.parent@gmail.com>
2020-05-17 18:15:16 +0200

    version 13.0.1

--- DotNet/Libraries/Lib_Framework_472/Properties/AssemblyInfo.cs
+++ DotNet/Libraries/Lib_Framework_472/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("13.0.0")]
-[assembly: AssemblyFileVersion("13.0.0")]
+[assembly: AssemblyVersion("13.0.1")]
+[assembly: AssemblyFileVersion("13.0.1")]
