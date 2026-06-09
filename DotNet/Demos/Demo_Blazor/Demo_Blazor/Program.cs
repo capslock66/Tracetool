2531790f7d855aba1f1ede699bb697b345b33fea
Eortc_TParent <thierry.parent@eortc.org>
2026-06-09 07:26:16 +0200

    Update blazor sample : web assembly vs server site traces

---
+++ DotNet/Demos/Demo_Blazor/Demo_Blazor/Program.cs
@@ -0,0 +1,35 @@
+using Demo_Blazor.Client.Pages;
+using Demo_Blazor.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents() 
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Demo_Blazor.Client._Imports).Assembly);
+
+app.Run();
